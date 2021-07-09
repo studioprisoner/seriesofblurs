f010c386de0f19f4b20bf3e57139148d13e6b877
Josh Illichmann <josh@studioprisoner.com>
2021-07-09 18:28:17 +1000

    starting movies

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -25,7 +25,21 @@ export default function Movies() {
                                 </div>
                                 <div className="relative">
                                     <div className="max-w-7xl mx-auto relative px-4 py-16 sm:px-6 sm:py-24 lg:py-24 lg:px-8">
-
+                                        <div>
+                                            <h3>Modern</h3>
+                                            <ul>
+                                                <li>Hereditary</li>
+                                                <li>The Witch</li>
+                                                <li>Black Swan</li>
+                                                <li>Babadook</li>
+                                                <li>Us</li>
+                                                <li>Green Room</li>
+                                                <li>Get Out</li>
+                                                <li>Raw</li>
+                                                <li>It Follows</li>
+                                                <li>A Quiet Place</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
