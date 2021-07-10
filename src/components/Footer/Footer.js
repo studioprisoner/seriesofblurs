6432389f973f1898ee7cd3d733edd09b9e304889
Josh Illichmann <josh@studioprisoner.com>
2021-07-10 18:07:48 +1000

    fixing things

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer class="overflow-y-auto">
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
                 <div class="mt-8 md:mt-0 md:order-1">
-                <p class="text-center text-white font-poppins">
+                <p class="text-center text-white font-poppins font-medium">
                     &copy; 2021 Series of Blurs, All rights reserved.
                 </p>
                 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Series of Blurs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div className="">
+    <div className="h-screen">
             <div className="relative h-full sm:h-screen bg-maroon-oak-700 sm:bg-transparent bg-gradient-to-t from-blue-charcoal-700">
             <div className="absolute right-auto sm:right-0 w-full sm:w-1/2">
                 <img
@@ -34,18 +34,18 @@ export default function Home() {
                         alt="Series of Blurs"
                     />
                 </h1>
-                <p className="mt-6 max-w-3xl text-3xl text-white font-poppins">Video Games &amp; Horror Movies</p>
+                <p className="mt-6 max-w-3xl text-3xl text-white font-poppins font-medium uppercase">Video Games &amp; Horror Movies</p>
                 <p className="mt-6 max-w-3xl text-xl text-white font-spacemono">Follow Series of Blurs for all your horror movie suggestions. This is the place to be for everyone who loves video games,  ghosts, aliens, monsters, flesh-eating-zombies and much more.</p>
                 <div class="py-6">
                   <a
                     href="/work" 
-                    className="w-full sm:w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-2xl text-white bg-scarlet-500 hover:bg-scarlet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-scarlet-500 font-poppins">
+                    className="w-full sm:w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-2xl text-white bg-scarlet-500 hover:bg-scarlet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-scarlet-500 font-poppins font-medium uppercase">
                     Work With Me
                 </a>
                 </div>
             </div>
             <section className="max-w-7xl mx-auto relative px-4 sm:px-6 lg:px-8" aria-labelledby="reach-heading">
-            <h2 className="text-3xl tracking-tight sm:text-4xl lg:text-5xl text-white font-poppins">Reach</h2>
+            <h2 className="text-3xl tracking-tight sm:text-4xl lg:text-5xl text-white font-poppins font-bold uppercase">Reach</h2>
             <div className="pt-10 grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
             <div className="relative rounded-lg border border-scarlet-500 px-6 py-5 flex items-center space-x-3 hover:bg-scarlet-500 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-scarlet-500">
                     <div className="flex-shrink-0">
