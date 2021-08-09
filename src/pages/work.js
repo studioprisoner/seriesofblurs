8ad3aa10c0802c37572c55206a169c40d48b1c49
Josh Illichmann <josh@studioprisoner.com>
2021-08-09 20:56:41 +1000

    added grid

--- src/pages/work.js
+++ src/pages/work.js
@@ -74,11 +74,12 @@ export default function work() {
             </Head>
             <Nav />
             <div className="mx-auto bg-maroon-oak-500 bg-transparent bg-gradient-to-t from-blue-charcoal-700">
-                <div className="h-56 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2 ">
+                <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-5">
+                <div>
                     <img src="/images/blurs_logo_2line.png" alt="" />
                 </div>
-                <div className="relative max-w-7xl mx-auto px-4 py-20 sm:px-6 lg:px-8 lg:py-16">
-                    <div className="md:ml-auto md:w-1/2 md:pl-10">
+                <div className="px-4 py-20 sm:px-6 lg:px-8 lg:py-16">
+                    <div>
                     <p className="mt-2 text-white text-3xl font-extrabold uppercase font-poppins tracking-tight sm:text-4xl">
                         Work with me.
                     </p>
@@ -86,6 +87,7 @@ export default function work() {
                     I'm a content creator and social media influencer. I work with brands and companies to help them reach their target audience through social media and content creation. If you have any questions about what I can offer you, or about working together on a project, don't hesitate to contact me!
                     </p>
                     </div>
+                </div>
                 </div>
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                     <div className="grid grid-cols-2 gap-8 lg:grid-cols-4">
