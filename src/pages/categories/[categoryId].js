d02cf8b1c179dd778425016ed5717e17a5f474cd
Josh Illichmann <josh@studioprisoner.com>
2021-10-21 20:18:56 +1100

    fixed getstatic paths

--- src/pages/categories/[categoryId].js
+++ src/pages/categories/[categoryId].js
@@ -1,7 +1,32 @@
+import Head from 'next/head'
+import Layout from 'components/Layout'
+
 export default function Categories({ category }) {
     return (
-        <div>
-            <h1>{category.title}</h1>
+        <div className="bg-maroon-oak-500">
+            <Head>
+                <title>Series of Blurs - Movies</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <div className="mx-auto">
+                    <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4">
+                        <div>
+                            <img src="/images/blurs_logo_2line.png" alt="Series of Blurs"/>
+                        </div>
+                        <div className="px-4 py-20 sm:px-6 lg:px-8 lg:py-16">
+                            <div>
+                                <p className="mt-2 text-white text-3xl font-extrabold uppercase font-poppins tracking-tight sm:text-4xl">Movies</p>
+                                <p className="mt-3 text-base font-spacemono text-gray-300">I have kept track of some of the best horror movies and came up with a curated list below. This list is constantly updated so make sure you keep coming back to check it out.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <section>
+                        {category.title}
+                    </section>
+                </div>
+            </Layout>
+
         </div>
     )
 }
@@ -9,7 +34,7 @@ export default function Categories({ category }) {
 
 export async function getStaticProps({ params }) {
     const categoryId = params.characterId
-    const results = await fetch(`https://seriesofblurs-cms.herokuapp.com/categories/${params.categoryId}`).then(res => res.json())
+    const results = await fetch(`https://seriesofblurs-cms.herokuapp.com/categories?=${params.categoryId}`).then(res => res.json())
 
     return {
         props: {
