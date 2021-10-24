8a1a9a2400c602467dbfa0dbe923dea538038ba7
Josh Illichmann <josh@studioprisoner.com>
2021-10-24 22:53:54 +1100

    added next.config

---
+++ next.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: securityHeaders
+            },
+            {
+                source: '/:path',
+                headers: securityHeaders
+            }
+        ]
+    }
+}
+
+const ContentSecurtityPolicy = `
+    default-src 'self' *.seriesofblurs-cms.herokuapp.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com;
+    child-src *.youtube.com *.google.com *.twitter.com;
+    style-src 'self' 'unsafe-inline' *.googleapis.com;
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self'
+`;
+
+const securityHeaders = [
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, '')
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload'
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()'
+    }
+];
